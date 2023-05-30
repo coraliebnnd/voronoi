c375bc8e4a861fd6ae9ea0acde269d3238ce4446
Coralie <119610564+coraliebnnd@users.noreply.github.com>
2023-05-30 22:34:32 +0100

    Add files via upload

---
+++ build/CMakeFiles/VoronoiApp.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "../bin/Debug/VoronoiApp"
+  "../bin/Debug/VoronoiApp.pdb"
+  "CMakeFiles/VoronoiApp.dir/src/SDL2_gfxPrimitives.c.o"
+  "CMakeFiles/VoronoiApp.dir/src/SDL2_gfxPrimitives.c.o.d"
+  "CMakeFiles/VoronoiApp.dir/src/application_ui.cpp.o"
+  "CMakeFiles/VoronoiApp.dir/src/application_ui.cpp.o.d"
+  "CMakeFiles/VoronoiApp.dir/src/main.cpp.o"
+  "CMakeFiles/VoronoiApp.dir/src/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/VoronoiApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
